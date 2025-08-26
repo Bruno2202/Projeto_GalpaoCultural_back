acf26f9942f97b6aba19e3360512772e031a752b
Leonardo <leonardoscabora@hotmail.com>
2025-08-25 23:54:24 -0300

    Initial commit

---
+++ src/main/java/Projeto_Padrao/CurricularizacaoApplication.java
@@ -0,0 +1,13 @@
+package Projeto_Padrao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurricularizacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurricularizacaoApplication.class, args);
+	}
+
+}

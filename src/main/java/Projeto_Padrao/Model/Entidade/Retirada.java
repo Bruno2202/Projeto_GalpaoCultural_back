ed623498aadaef6ddb3c59a526d9dbc9e55f30db
Leonardo <leonardoscabora@hotmail.com>
2025-08-26 18:21:55 -0300

    Criação da entidade responsavel em coletar os dados do usuário.

---
+++ src/main/java/Projeto_Padrao/Model/Entidade/Retirada.java
@@ -0,0 +1,37 @@
+package Projeto_Padrao.Model.Entidade;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(schema = "public", name = "Livros")
+public class Retirada {
+
+    @Id
+    @Column(name = "CELULAR", nullable = false)
+    String celular;
+
+    @Column(name = "NOME", nullable = false)
+    String nome;
+
+    @Column(name = "LIVRO", nullable = false)
+    String livro;
+
+    @Column(name = "AUTOR", nullable = false)
+    String autor;
+
+    @Column(name = "EMPRESTIMO", nullable = false)
+    LocalDate emprestimo;
+
+    @Column(name = "DEVOLUCAO")
+    LocalDate devolucao;
+}
